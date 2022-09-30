338c776c33c4c495eb9b3a284cb98a18317a97fa
Ryan Lowe <somebody480@gmail.com>
2022-09-30 15:18:31 -0400

    match (string)

--- match (string).js
+++ match (string).js
@@ -1,27 +1,26 @@
 /*
-includes() 
+match() 
 
 Type: String
 
 Description: 
-Searches the string for a given substring, returning the index of the first occurance of the substring if found.
-Returns -1 if not found, if not provided an argument returns 0.
-Can be given a second Number argument to begin searching from a specific index.
+Returns values that match a given regular expression in the current string as an array containing each match.
+Returns null if no match is found.
+If given no argument, returns an array with an empty string ""
 
-Time complexity: O(n) where n is the number of characters within the searched string.
+Time complexity: O(n) to O(2^m) where m is the regular expression size.
 */
 
-let str = "A string.";
-let substring = "string";
+let str = "A string containing SOME uppercase letters and numbers: 12345";
 
 // Example 1
-// Searches for 'string' within str, returning the index that 'string' begins at: 2. The search begins at index 0 by default, therefore searching the entire string.
-console.log(`Example 1: ${str.indexOf(substring)}`);
+// Returns an array of values that are uppercase and alphabetical within the string
+console.log(`Example 1: ${str.match(/[A-Z]/g)}`);
 
 // Example 2
-// Searches for 'string' within str beginning at index 4 ('r'), returning -1 because the full substring ('string') is not found past index 4.
-console.log(`Example 2: ${str.indexOf(substring, 4)}`);
+// Returns an array of values that are digits within the string
+console.log(`Example 1: ${str.match(/[0-9]/g)}`);
 
 // Example 3
-// Searches for 'string' within str beginning at index 1 (' '), returning 2 because the full substring ('string') is found past index 1, beginning at index 2.
-console.log(`Example 3: ${str.indexOf(substring, 1)}`);
+// Returns an array with an empty string, equivalent to using match(/(?:)/)
+console.log(`Example 1: ${str.match()}`);
