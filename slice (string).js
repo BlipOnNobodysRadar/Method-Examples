52e5c2ff9add8630f528c4ede89f41321e22f370
Ryan Lowe <somebody480@gmail.com>
2022-10-15 14:06:15 -0400

    slice (string) method

---
+++ slice (string).js
@@ -0,0 +1,29 @@
+/*
+slice() 
+
+Type: String
+
+Description: 
+Extracts (slices) a section of a string and returns it as a new string. Doesn't modify the original string.
+Taking in a starting index and optionally an ending index as parameters.
+
+Slices up to the ending index, but does not include the ending index in the slice.
+
+If given a negative index, begins from the back of the string.
+
+Time complexity: O(n)
+*/
+
+let str = "A string.";
+
+// Example 1
+// Returns a new string starting from the 2nd index ('s') in the original string.
+console.log(`Example 1: ${str.slice(2)}`);
+
+// Example 2
+// Returns a new string from the characters between index 2 (inclusive) and index 5 (noninclusive)
+console.log(`Example 1: ${str.slice(2, 5)}`);
+
+// Example 3
+// Returns a new string between index 2 and 4 indexes before the end of the string.
+console.log(`Example 1: ${str.slice(2, -4)}`);
