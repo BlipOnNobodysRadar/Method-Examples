48541a9915f304a9c20f13f1484dcb915f987ccb
Ryan Lowe <somebody480@gmail.com>
2022-11-10 11:45:51 -0500

    Fix example text

--- Set/add.js
+++ Set/add.js
@@ -5,6 +5,7 @@ Type: Set
 
 Description: 
 Inserts a new non-duplicate element into the Set object. Returns the updated Set object.
+.add() is chainable
 
 Time complexity: O(1)
 */
@@ -14,33 +15,33 @@ let aSet = new Set();
 //! Look at the .values() method to understand what's done to log the values in the Set.
 
 // Example 1
-// Adds 5 (number) to the set; (need to )
+// Adds 5 (number) to the set
 aSet.add(5);
 // Code below is just to display the result
 let values = aSet.values();
-let example1 = "Example1 1: ";
+let example1 = "Example 1: ";
 for (value of values) {
   example1 += `${value} (${typeof value}), `;
 }
 console.log(example1);
 
 // Example 2
-// Adds 5 (string) to the set;
-aSet.add("5");
+// Adds 5 (string) to the set and seven (string) to the set by chaining the method
+aSet.add("5").add("seven");
 // Code below is just to display the result
 values = aSet.values();
-let example2 = "Example1 1: ";
+let example2 = "Example 2: ";
 for (value of values) {
   example2 += `${value} (${typeof value}), `;
 }
 console.log(example2);
 
 // Example 3
-// Since 5 (number) already exists in the set, the Set remains the same;
+// Since 5 (number) already exists in the set, the Set remains the same
 aSet.add(5);
 // Code below is just to display the result
 values = aSet.values();
-let example3 = "Example1 1: ";
+let example3 = "Example 3: ";
 for (value of values) {
   example3 += `${value} (${typeof value}), `;
 }
